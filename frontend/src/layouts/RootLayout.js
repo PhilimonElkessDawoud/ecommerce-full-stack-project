bdb0697380e05ad05ba200800b6cd9b7911de5d7
Mina Magdy <72947628+amwopps@users.noreply.github.com>
2023-01-09 16:20:43 +0200

    Fixed uneseccary warnings

--- frontend/src/layouts/RootLayout.js
+++ frontend/src/layouts/RootLayout.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import {Header, Footer} from 'C:/xampp/htdocs/ecommerce-full-stack-project/project/src/sections/index'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from "../sections/index";
 
 const RootLayout = () => {
-  return (
-    <div className='root-layout'>
-        <Header/>
-        <Outlet/>
-        <Footer/>
-    </div>
-  )
-}
+    return (
+        <div className="root-layout">
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+};
 
-export default RootLayout
+export default RootLayout;
